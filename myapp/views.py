3a8dce081f63e721b1624a18eaba5d2e919952ce
SyBex <arunkumar.s@mnw.co.in>
2018-04-20 17:55:13 +0530

    removing .pyc

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>Hello World</h1>");
+	if request.method == "GET":
+		return render(request,'index.html');
+	elif request.method == "POST":
+		return render(request,'index.html');
