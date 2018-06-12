1d18627b5388e9c18e5246bc76bff05850a07792
arunsybex <arunsathamk@gmail.com>
2018-06-12 22:16:17 +0530

    home work

--- myapp/views.py
+++ myapp/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 from django.conf import settings
 from PIL import Image
 from pytesser import *
-import os
+import os,shutil
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 def index(request):
 	if request.method == "GET":
-		# image_file = '/home/mnwsoftware/projects/heroku/myapp/fnord.tif'
+		# image_file = '/home/arunkumar/projects/heroku/myapp/fnord.tif'
 		# im = Image.open(image_file)
 		# text = image_to_string(im)
 		# text = image_file_to_string(image_file)
@@ -18,11 +19,18 @@ def index(request):
 		return render(request,'index.html');
 	elif request.method == "POST":
 		crfs_mid = request.POST["csrfmiddlewaretoken"];
-		print request.FILES.getlist('file')
-		for wfile in request.FILES.getlist('file'):
-			im = Image.open(wfile)
-			text = image_to_string(im)
-			# text = image_file_to_string(im)
-			# text = image_file_to_string(im, graceful_errors=True)
-			print text
-		return render(request,'index.html');
+		output = "";
+		if request.method == 'POST' and request.FILES['file']:
+			for wfile in request.FILES.getlist('file'):
+				myfile = wfile
+				fs = FileSystemStorage()
+				filename = fs.save(crfs_mid+"/"+myfile.name, myfile)
+				uploaded_file_url = fs.url(filename)
+				# image_file = settings.BASE_DIR+uploaded_file_url
+				# im = Image.open(image_file)
+				# text = image_to_string(im)
+				# text = image_file_to_string(image_file)
+				# text = image_file_to_string(image_file, graceful_errors=True)
+				# output = output + text
+		shutil.rmtree(settings.BASE_DIR+'/media/'+crfs_mid)
+		return HttpResponse(output)
--- sybex/settings.py
+++ sybex/settings.py
@@ -122,6 +122,9 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
